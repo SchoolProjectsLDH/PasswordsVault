1b0411c21874eae77ac6c837494d580e681879da
Kggupta <kgupt1@ocdsb.ca>
2019-12-14 16:44:23 -0500

    change user window

--- PasswordsVault/src/main/java/com/theborginc/passwordsvault/Auth/Configs.java
+++ PasswordsVault/src/main/java/com/theborginc/passwordsvault/Auth/Configs.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package com.theborginc.passwordsvault.Auth;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 
@@ -13,6 +15,18 @@ import java.nio.file.Files;
  * @author keshavgupta
  */
 public class Configs {
+    
+    private static void write(String[] parrameters) throws IOException{
+        BufferedWriter outputWriter = null;
+        outputWriter = new BufferedWriter(new FileWriter("./src/main/resources/config.dat"));
+        for (int i = 0; i < parrameters.length; i++) {
+            outputWriter.write(parrameters[i]+"");
+            outputWriter.newLine();
+        }
+        outputWriter.flush();  
+        outputWriter.close();  
+    }
+    
     public String getUsername() throws IOException{
         String[] lines = Files.readAllLines(new File("./src/main/resources/config.dat").toPath()).toArray(new String[0]);
         return lines[0];
@@ -21,5 +35,17 @@ public class Configs {
     public String getPassword() throws IOException{
         String[] lines = Files.readAllLines(new File("./src/main/resources/config.dat").toPath()).toArray(new String[0]);
         return lines[1];
+    }
+    
+    public void setUsername(String newUsername) throws IOException{
+        String[] lines = Files.readAllLines(new File("./src/main/resources/config.dat").toPath()).toArray(new String[0]);
+        lines[0] = newUsername;
+        write(lines);
+    }
+    
+    public void setPassword(String newPass) throws IOException{
+        String[] lines = Files.readAllLines(new File("./src/main/resources/config.dat").toPath()).toArray(new String[0]);
+        lines[1] = newPass;
+        write(lines);
     }
 }
