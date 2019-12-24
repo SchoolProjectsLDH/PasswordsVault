e9f6b6248d268e69e0e0d2caacca256c062b2182
Kggupta <kgupt1@ocdsb.ca>
2019-12-24 12:38:09 -0500

    commented pass generator class

--- PasswordsVault/src/main/java/com/theborginc/passwordsvault/Auth/PasswordGenerator.java
+++ PasswordsVault/src/main/java/com/theborginc/passwordsvault/Auth/PasswordGenerator.java
@@ -10,12 +10,12 @@ import java.util.Random;
  */
 public final class PasswordGenerator {
     
-    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
-    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final String DIGITS = "0123456789";
-    private static final String PUNCTUATION = "!@#$%&*=,./";
-    private static final String ADVANCEDPUNC = "()_+-[]|?<>";
-    private boolean useLowerCase;
+    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";//all lowercase letters
+    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";//all uppercase letters
+    private static final String DIGITS = "0123456789";//all numbers
+    private static final String PUNCTUATION = "!@#$%&*=,./";//all basic symbols
+    private static final String ADVANCEDPUNC = "()_+-[]|?<>";//all advanced symbols
+    private boolean useLowerCase;//Booleans to determine whether the user wants said parameter
     private boolean useUpperCase;
     private boolean useNumbers;
     private boolean useBasicSymbols;
@@ -26,7 +26,7 @@ public final class PasswordGenerator {
     }
 
     private PasswordGenerator(PasswordGeneratorBuilder builder) {
-        this.useLowerCase = builder.useLowerCase;
+        this.useLowerCase = builder.useLowerCase;//init as the builder values (given on creation)
         this.useUpperCase = builder.useUpperCase;
         this.useNumbers = builder.useNumbers;
         this.useBasicSymbols = builder.useBasicSymbols;
@@ -35,14 +35,14 @@ public final class PasswordGenerator {
 
     public static class PasswordGeneratorBuilder {
 
-        private boolean useLowerCase;
+        private boolean useLowerCase;//creating builder
         private boolean useUpperCase;
         private boolean useNumbers;
         private boolean useBasicSymbols;
         private boolean useAdvSymbols;
 
         public PasswordGeneratorBuilder() {
-            this.useLowerCase = false;
+            this.useLowerCase = false;//initialize as false for all parameters
             this.useUpperCase = false;
             this.useNumbers = false;
             this.useBasicSymbols = false;
@@ -50,45 +50,45 @@ public final class PasswordGenerator {
         }
 
         public PasswordGeneratorBuilder useLowerCase(boolean useLower) {
-            this.useLowerCase = useLower;
+            this.useLowerCase = useLower;//if user wants lowercase return the choice
             return this;
         }
 
         public PasswordGeneratorBuilder useUpperCase(boolean useUpper) {
-            this.useUpperCase = useUpper;
+            this.useUpperCase = useUpper;//if user wants uppercase return the choice
             return this;
         }
 
         public PasswordGeneratorBuilder useNumbers(boolean useDigits) {
-            this.useNumbers = useDigits;
+            this.useNumbers = useDigits;//see above
             return this;
         }
 
         public PasswordGeneratorBuilder useBasicSymbols(boolean usePunctuation) {
-            this.useBasicSymbols = usePunctuation;
+            this.useBasicSymbols = usePunctuation;//see above
             return this;
         }
         
         public PasswordGeneratorBuilder useAdvSymbols(boolean useAdvPunctuation){
-            this.useAdvSymbols = useAdvPunctuation;
+            this.useAdvSymbols = useAdvPunctuation;//see above
             return this;
         }
 
-        public PasswordGenerator build() {
+        public PasswordGenerator build() {//main builder for generator
             return new PasswordGenerator(this);
         }
     }
 
-    public String generate(int length) {
+    public String generate(int length) {//actual generator method. Takes length
         if (length <= 0) {
             return "";
         }
 
-        StringBuilder password = new StringBuilder(length);
-        Random random = new Random(System.nanoTime());
+        StringBuilder password = new StringBuilder(length);//a password of length given
+        Random random = new Random(System.nanoTime());//randomizer
 
-        List<String> charParameters = new ArrayList<>(4);
-        if (useLowerCase) {
+        List<String> charParameters = new ArrayList<>(5);
+        if (useLowerCase) {//add parameters of the choice to the generator
             charParameters.add(LOWER);
         }
         if (useUpperCase) {
@@ -104,11 +104,11 @@ public final class PasswordGenerator {
             charParameters.add(ADVANCEDPUNC);
         }
 
-        for (int i = 0; i < length; i++) {
-            String charCategory = charParameters.get(random.nextInt(charParameters.size()));
-            int position = random.nextInt(charCategory.length());
-            password.append(charCategory.charAt(position));
+        for (int i = 0; i < length; i++) {//generate each character accross the length
+            String charCategory = charParameters.get(random.nextInt(charParameters.size()));//get random value from the parameter size
+            int position = random.nextInt(charCategory.length());//randomize position
+            password.append(charCategory.charAt(position));//set the randomized character at randomized position
         }
-        return new String(password);
+        return new String(password);//return the built password
     }
 }
