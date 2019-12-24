bdf46c99bdc0cf50c56d104e4707ae2594b4f588
Kggupta <kgupt1@ocdsb.ca>
2019-12-24 12:52:20 -0500

    comment database editor class

--- PasswordsVault/src/main/java/com/theborginc/passwordsvault/Auth/DatabaseEditor.java
+++ PasswordsVault/src/main/java/com/theborginc/passwordsvault/Auth/DatabaseEditor.java
@@ -14,77 +14,77 @@ import org.json.simple.parser.JSONParser;
  * @author keshavgupta
  */
 public class DatabaseEditor {
-    JSONArray passwords;
-    Configs config = new Configs();
+    JSONArray passwords;//passwords array
     
-    private final static String PASSWORD_TEXT = "./src/main/resources/passwords.txt";
+    private final static Configs CONFIG = new Configs();//config constant
+    private final static String PASSWORD_TEXT = "./src/main/resources/passwords.txt";//paths to encrypted and decrypted files
     private final static String PASSWORD_JSON = "./src/main/resources/passwords.json";
     
-    public void JsonParse(String accountType, String title, String username, String password) throws Exception{
+    public void JsonParse(String accountType, String title, String username, String password) throws Exception{//parse the given parameters to array
         JSONParser jsonParser = new JSONParser();
-        EncryptDecrypt cipherHandler = new EncryptDecrypt();
+        EncryptDecrypt cipherHandler = new EncryptDecrypt();//controls cipher
         
-        cipherHandler.encrypt(config.getSecretKey(), Cipher.DECRYPT_MODE, PASSWORD_TEXT, PASSWORD_JSON);
+        cipherHandler.encrypt(CONFIG.getSecretKey(), Cipher.DECRYPT_MODE, PASSWORD_TEXT, PASSWORD_JSON);//decrypt the text file to json file
         
-        try (FileReader reader = new FileReader(PASSWORD_JSON)){
+        try (FileReader reader = new FileReader(PASSWORD_JSON)){//read json file
             //Read JSON file
-            Object obj = jsonParser.parse(reader);
+            Object obj = jsonParser.parse(reader);//send to json array
             passwords = (JSONArray) obj;
         } catch (Exception e){
             System.out.println("Invalid json array");
         }
-        passwords.add(new Account(accountType,title,username,password));         
-        writeToFile(passwords);
+        passwords.add(new Account(accountType,title,username,password));//add parameters to json array      
+        writeToFile(passwords);//write new json array to json file
         
-        cipherHandler.encrypt(config.getSecretKey(), Cipher.ENCRYPT_MODE, PASSWORD_JSON, PASSWORD_TEXT);
-        cipherHandler.clear();
+        cipherHandler.encrypt(CONFIG.getSecretKey(), Cipher.ENCRYPT_MODE, PASSWORD_JSON, PASSWORD_TEXT);//encrypt json file
+        cipherHandler.clear();//erase json file
     }
     
     
     public static void writeToFile(JSONArray passwords) {
-        try (FileWriter file = new FileWriter(PASSWORD_JSON)) {
-            file.write(passwords.toString());
+        try (FileWriter file = new FileWriter(PASSWORD_JSON)) {//output file writer
+            file.write(passwords.toString());//write json array to file
             file.flush();
         } catch (IOException e) {
             System.out.println("Failed to write to file. FNF");
         }
     }
     
-    public void removeAccount(int location) throws Exception{
+    public void removeAccount(int location) throws Exception{//deletes account
         JSONParser jsonParser = new JSONParser();
         EncryptDecrypt cipherHandler = new EncryptDecrypt();
         
-        cipherHandler.encrypt(config.getSecretKey(), Cipher.DECRYPT_MODE, PASSWORD_TEXT, PASSWORD_JSON);
+        cipherHandler.encrypt(CONFIG.getSecretKey(), Cipher.DECRYPT_MODE, PASSWORD_TEXT, PASSWORD_JSON);//decrypt json array to json file
         
-        try (FileReader reader = new FileReader(PASSWORD_JSON)){
+        try (FileReader reader = new FileReader(PASSWORD_JSON)){//read json file
             //Read JSON file
-            Object obj = jsonParser.parse(reader);
+            Object obj = jsonParser.parse(reader);//send json file to array
             passwords = (JSONArray) obj;
         } catch (Exception e){
             System.out.println("Account removal failed due to invalid json object");
         }
-        passwords.remove(location);
-        writeToFile(passwords);
+        passwords.remove(location);//remove from given location
+        writeToFile(passwords);//write updated passwords array
         
-        cipherHandler.encrypt(config.getSecretKey(), Cipher.ENCRYPT_MODE, PASSWORD_JSON, PASSWORD_TEXT);
-        cipherHandler.clear();
+        cipherHandler.encrypt(CONFIG.getSecretKey(), Cipher.ENCRYPT_MODE, PASSWORD_JSON, PASSWORD_TEXT);//encrypt json file
+        cipherHandler.clear();//erase json file
     }
     
 }
 
 class Account implements JSONAware {
-    String accountType;
+    String accountType;//Parameters in each object
     String title;
     String username;
     String password;
-    Account(String type, String titleInfo, String Username, String Password){
+    Account(String type, String titleInfo, String Username, String Password){//initialize account with given types
         this.accountType = type;
         this.title = titleInfo;
         this.username = Username;
         this.password = Password;
     }
     @Override
-    public String toJSONString() {
+    public String toJSONString() {//build the object json string syntax
         StringBuilder sb = new StringBuilder();
         sb.append("{");
         sb.append("\"account\"");
@@ -103,7 +103,7 @@ class Account implements JSONAware {
         sb.append(":");
         sb.append("\"").append(JSONObject.escape(password)).append("\"");
         sb.append("}");
-        return sb.toString();
+        return sb.toString();//combine to string
     }   
     
 }
