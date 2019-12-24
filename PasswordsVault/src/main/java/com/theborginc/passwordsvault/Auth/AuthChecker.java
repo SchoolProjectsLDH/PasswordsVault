60d5e8223831662b1258af6aa21ac6a2a06d6552
Kggupta <kgupt1@ocdsb.ca>
2019-12-24 12:58:00 -0500

    comment auth checker class

--- PasswordsVault/src/main/java/com/theborginc/passwordsvault/Auth/AuthChecker.java
+++ PasswordsVault/src/main/java/com/theborginc/passwordsvault/Auth/AuthChecker.java
@@ -7,14 +7,14 @@ import java.io.IOException;
  * @author keshavgupta
  */
 public class AuthChecker {
-    private final GoogleAuthMaker gAuth = new GoogleAuthMaker();
-    private final Configs config = new Configs();
+    private final GoogleAuthMaker gAuth = new GoogleAuthMaker();//Google auth class
+    private final Configs config = new Configs();//config reader
     
-    public boolean compareValues(String username, String password, String twoFactor) throws IOException{
+    public boolean compareValues(String username, String password, String twoFactor) throws IOException{//compare given values against actual values
         return(username.equals(config.getUsername()) && password.equals(config.getPassword()) && twoFactor.equals(gAuth.getCode()));
     }
     
     public String getTFact() throws IOException{
-        return gAuth.getCode();
+        return gAuth.getCode();//get the 6digit code from google auth maker
     }
 }
