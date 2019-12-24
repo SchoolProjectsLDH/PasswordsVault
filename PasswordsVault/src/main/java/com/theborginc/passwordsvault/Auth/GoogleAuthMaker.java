492fc3419beb8f32a0143b9b9b4bed35d7a5d33b
Kggupta <kgupt1@ocdsb.ca>
2019-12-23 19:03:19 -0500

    replaced file name with static constant

--- PasswordsVault/src/main/java/com/theborginc/passwordsvault/Auth/GoogleAuthMaker.java
+++ PasswordsVault/src/main/java/com/theborginc/passwordsvault/Auth/GoogleAuthMaker.java
@@ -13,9 +13,10 @@ import java.security.GeneralSecurityException;
  * @author keshavgupta
  */
 public class GoogleAuthMaker {
+    private static final String CONFIG_FILE = "./src/main/resources/config.dat";
     private static void write(String[] parrameters) throws IOException{
         BufferedWriter outputWriter;
-        outputWriter = new BufferedWriter(new FileWriter("./src/main/resources/config.dat"));
+        outputWriter = new BufferedWriter(new FileWriter(CONFIG_FILE));
         for (String parrameter : parrameters) {
             outputWriter.write(parrameter + "");
             outputWriter.newLine();
@@ -25,7 +26,7 @@ public class GoogleAuthMaker {
     }
     
     public String getCode() throws IOException{
-        String[] lines = Files.readAllLines(new File("./src/main/resources/config.dat").toPath()).toArray(new String[0]);
+        String[] lines = Files.readAllLines(new File(CONFIG_FILE).toPath()).toArray(new String[0]);
         try{
             String code = TimeBasedOneTimePasswordUtil.generateCurrentNumberString(lines[2]);
             return code;
@@ -36,7 +37,7 @@ public class GoogleAuthMaker {
     }
     
     public String newCode() throws IOException{
-        String[] lines = Files.readAllLines(new File("./src/main/resources/config.dat").toPath()).toArray(new String[0]);
+        String[] lines = Files.readAllLines(new File(CONFIG_FILE).toPath()).toArray(new String[0]);
         String base32Secret = TimeBasedOneTimePasswordUtil.generateBase32Secret();
         lines[2] = base32Secret;
         write(lines);
