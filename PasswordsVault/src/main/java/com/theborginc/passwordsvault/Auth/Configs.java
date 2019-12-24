b260e745e58193a8a9ff314f9397ab518dab5a1b
Kggupta <kgupt1@ocdsb.ca>
2019-12-24 12:56:56 -0500

    comment config class

--- PasswordsVault/src/main/java/com/theborginc/passwordsvault/Auth/Configs.java
+++ PasswordsVault/src/main/java/com/theborginc/passwordsvault/Auth/Configs.java
@@ -12,63 +12,63 @@ import java.nio.file.Files;
  */
 public class Configs {
     
-    private static void write(String[] parameters) throws IOException{
-        BufferedWriter outputWriter;
+    private static void write(String[] parameters) throws IOException{//write to config file 
+        BufferedWriter outputWriter;//output file
         outputWriter = new BufferedWriter(new FileWriter("./src/main/resources/config.dat"));
-        for (String parrameter : parameters) {
-            outputWriter.write(parrameter + "");
-            outputWriter.newLine();
+        for (String parameter : parameters) {//for each parameter write to line
+            outputWriter.write(parameter + "");//write parameter
+            outputWriter.newLine();//next line
         }
-        outputWriter.flush();  
+        outputWriter.flush();//safely close
         outputWriter.close();  
     }
     
-    private String[] readFile() throws IOException{
+    private String[] readFile() throws IOException{//read file helper method
         return Files.readAllLines(new File("./src/main/resources/config.dat").toPath()).toArray(new String[0]);
     }
     
-    public String getUsername() throws IOException{
+    public String getUsername() throws IOException{//get the actual username
         String[] lines = readFile();
-        return lines[0];
+        return lines[0];//send username
     }
 
-    public String getPassword() throws IOException{
+    public String getPassword() throws IOException{//get the actual password
         String[] lines = readFile();
-        return lines[1];
+        return lines[1];//send password
     }
     
-    public int getStrikes() throws IOException{
+    public int getStrikes() throws IOException{//get number of strikes
         String[] lines = readFile();
-        return Integer.parseInt(lines[3]);
+        return Integer.parseInt(lines[3]);//send strikes as int
     }
     
-    public void addStrike() throws IOException{
+    public void addStrike() throws IOException{//add 1 to strikes
         String[] lines = readFile();
-        lines[3] = String.valueOf(Integer.parseInt(lines[3])+1);
+        lines[3] = String.valueOf(Integer.parseInt(lines[3])+1);//set strikes to + 1 itself
+        write(lines);//write to file
+    }
+    
+    public void resetStrike() throws IOException{//reset strike counter
+        String[] lines = readFile();
+        lines[3] = "0";//reset strikes to 0
         write(lines);
     }
     
-    public void resetStrike() throws IOException{
+    public void setUsername(String newUsername) throws IOException{//reset username
         String[] lines = readFile();
-        lines[3] = "0";
-        write(lines);
+        lines[0] = newUsername;//set username as given value
+        write(lines);//write updated value
     }
     
-    public void setUsername(String newUsername) throws IOException{
+    public void setPassword(String newPass) throws IOException{//reset password
         String[] lines = readFile();
-        lines[0] = newUsername;
-        write(lines);
+        lines[1] = newPass;//set pass as given value
+        write(lines);//write updated value
     }
     
-    public void setPassword(String newPass) throws IOException{
+    public String getSecretKey() throws IOException{//get key for encryptor
         String[] lines = readFile();
-        lines[1] = newPass;
-        write(lines);
-    }
-    
-    public String getSecretKey() throws IOException{
-        String[] lines = readFile();
-        return lines[4];
+        return lines[4];//return key as string
 
     }
 }
